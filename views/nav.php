8da2adc716dc8464dcca4b35e4249abb9c7ed36b
aurelienheude <aurelienheudepro@gmail.com>
2021-01-27 17:23:50 +0100

    ajout des liens pour l'appli

--- views/nav.php
+++ views/nav.php
@@ -6,6 +6,9 @@
 				<img src="../assets/img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
 				<span>Lovely love</span>
 				</a>
+				<a href="users.php">Mon profil</a>
+				<a href="lovers.php">Lovers</a>
+				<a href="deconnexion.php">Me deconnecter</a>
 			</div>
 		</div>
 	</nav>
