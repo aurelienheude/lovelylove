553c46ae9c3873d519e7350523c9f1a4bf72d74d
aurelienheude <aurelienheudepro@gmail.com>
2021-01-27 23:21:16 +0100

    ajout de banner dans chaque page

---
+++ views/banner_user.php
@@ -0,0 +1,9 @@
+<section class="bg-success container d-flex m-0 p-0">
+		<div class="row mx-auto">
+			<div class="col-md-12 py-5">
+				<div class="">
+					<img class="Profil_pic_circle rounded-circle" src="../assets/img/300.png" width="150px" height="150px" />
+				</div>
+			</div>
+		</div>
+</section>
