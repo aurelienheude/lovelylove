42f995e49b8d31eb0ad0299d715bdb00863d78ab
aurelienheude <aurelienheudepro@gmail.com>
2021-01-27 23:21:57 +0100

    Modification majeur de la nav

--- views/nav.php
+++ views/nav.php
@@ -1,14 +1,22 @@
-	<!-- Image and text -->
-	<nav class="NavSection container m-0 navbar">
-		<div class="row mx-auto">
-			<div class="col-md-12">
-				<a class="navbar-brand" href="#">
-				<img src="../assets/img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
-				<span>Lovely love</span>
-				</a>
-				<a href="users.php">Mon profil</a>
-				<a href="lovers.php">Lovers</a>
-				<a href="deconnexion.php">Me deconnecter</a>
-			</div>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark" aria-label="Fourth navbar example">
+	<div class="container-fluid">
+		<a class="navbar-brand" href="#">
+			<img src="../assets/img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+			<span>Lovely love</span>
+		</a>
+		<button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="navbar-collapse collapse" id="navbarsExample04" style="">
+			<ul class="navbar-nav me-auto mb-2 mb-md-0">
+				<li class="nav-item active">
+					<a class="nav-link" aria-current="page" href="users.php">Mes informations</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="lovers.php">Nos célibataires</a>
+				</li>
+			</ul>
 		</div>
-	</nav>
+	</div>
+</nav>
