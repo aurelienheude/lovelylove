81e0a51056636a7bfe93bded54377422d400a272
aurelienheude <aurelienheudepro@gmail.com>
2021-01-27 16:41:33 +0100

    V1 système de cards foreach

--- controllers/lovers_controller.php
+++ controllers/lovers_controller.php
@@ -19,9 +19,9 @@ $tab_all_member =
     [
     "lastname" => "toto",
     "firstname" => "Marius", 
-    "age" => 0, 
+    "age" => 20, 
     "gender" => "homme", 
-    "mail" => "mariuskoko@gmail.com",
+    "mail" => "toto@gmail.com",
     "zipcode" => 76620,
     "picture" => "../assets/img/300.png",
     "description" => "je mange des cookies"
@@ -31,7 +31,23 @@ $tab_all_member =
 
 foreach($tab_all_member as $key => $value)
 {
-    var_dump($value["lastname"]);
-}
 
 ?>
+
+<div class="col-md-3 pb-5">
+                <div class="card">
+                    <img class="card-img-top" src="<?php echo $value["picture"];?>" alt="Card image cap">
+
+                    <div class="card-body">
+                        <h5 class="card-title text-center"><?php echo $value["lastname"].", ".$value["age"]; ?></h5>
+                            <ul>
+                                <li>Code postal : <?php echo $value["zipcode"]; ?></li>
+                                <li>membre vérifié : </li>
+                                <li>Disponible en ligne : </li>
+                            </ul>
+                    </div>
+                </div>
+            </div>
+
+
+<?php } ?>
