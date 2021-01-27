7930a12a2461c5e496f79a589aef262c568190cd
aurelienheude <aurelienheudepro@gmail.com>
2021-01-27 14:07:47 +0100

    début page lover, ajout de modif et des cards taff  pour l'aprem

--- controllers/lovers_controller.php
+++ controllers/lovers_controller.php
@@ -1,7 +1,20 @@
 <?php 
 
+$tab_all_member = 
+[
+    ['lastname' => 'Mathilde', 'mail' => ''],
+    ['firstname' => 'Pierre', 'mail' => 'pierre.giraud@edhec.com'],
+    ['age' => 'Amandine', 'mail' => 'amandine@lp.fr'],
+    ['gender' => 'Amandine', 'mail' => 'amandine@lp.fr'],
+    ['mail' => 'Amandine', 'mail' => 'amandine@lp.fr'],
+    ['zipcode' => 'Amandine', 'mail' => 'amandine@lp.fr'],
+    ['picture' => 'Amandine', 'mail' => 'amandine@lp.fr'],  
+    ['description' => '', '']
+];
 
-
-
+for ($cursor = 0; $cursor < count($tab_all_member); $cursor++)
+{
+    echo $tab_all_member[$cursor];
+}
 
 ?>
--- views/lovers.php
+++ views/lovers.php
@@ -1,7 +1,219 @@
-<section class="container">
-    <div class="row">
-        <div class="col-md-12">
-            
+<?php include '../controllers/cookies.php' ?>
+<?php include("../controllers/index_controller.php"); ?>
+
+<!-- DEBUT DE PAGE -->
+
+<?php include("core_views/head.php"); ?>
+
+<div class='container vh-100'>
+
+    <?php include("nav.php") ?>
+
+    <section class="bg-success container d-flex m-0 p-0">
+        <div class="row mx-auto">
+            <div class="col-md-12 py-5">
+                picture
+            </div>
         </div>
-    </div>
-</section>
+    </section>
+
+    <section class="container m-0 pt-4">
+        <div class="row">
+            <div class="col-md-3 pb-5">
+                <div class="card">
+                    <img class="card-img-top" src="../assets/img/300.png" alt="Card image cap">
+
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Clara, 26</h5>
+                        <ul>
+                            <li>Le havre, 76</li>
+                            <li>membre vérifié</li>
+                            <li>Disponible en ligne</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 pb-5">
+                <div class="card">
+                    <img class="card-img-top" src="../assets/img/300.png" alt="Card image cap">
+
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Clara, 26</h5>
+                        <ul>
+                            <li>Le havre, 76</li>
+                            <li>membre vérifié</li>
+                            <li>Disponible en ligne</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 pb-5">
+                <div class="card">
+                    <img class="card-img-top" src="../assets/img/300.png" alt="Card image cap">
+
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Clara, 26</h5>
+                        <ul>
+                            <li>Le havre, 76</li>
+                            <li>membre vérifié</li>
+                            <li>Disponible en ligne</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 pb-5">
+                <div class="card">
+                    <img class="card-img-top" src="../assets/img/300.png" alt="Card image cap">
+
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Clara, 26</h5>
+                        <ul>
+                            <li>Le havre, 76</li>
+                            <li>membre vérifié</li>
+                            <li>Disponible en ligne</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 pb-5">
+                <div class="card">
+                    <img class="card-img-top" src="../assets/img/300.png" alt="Card image cap">
+
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Clara, 26</h5>
+                        <ul>
+                            <li>Le havre, 76</li>
+                            <li>membre vérifié</li>
+                            <li>Disponible en ligne</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 pb-5">
+                <div class="card">
+                    <img class="card-img-top" src="../assets/img/300.png" alt="Card image cap">
+
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Clara, 26</h5>
+                        <ul>
+                            <li>Le havre, 76</li>
+                            <li>membre vérifié</li>
+                            <li>Disponible en ligne</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 pb-5">
+                <div class="card">
+                    <img class="card-img-top" src="../assets/img/300.png" alt="Card image cap">
+
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Clara, 26</h5>
+                        <ul>
+                            <li>Le havre, 76</li>
+                            <li>membre vérifié</li>
+                            <li>Disponible en ligne</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 pb-5">
+                <div class="card">
+                    <img class="card-img-top" src="../assets/img/300.png" alt="Card image cap">
+
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Clara, 26</h5>
+                        <ul>
+                            <li>Le havre, 76</li>
+                            <li>membre vérifié</li>
+                            <li>Disponible en ligne</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 pb-5">
+                <div class="card">
+                    <img class="card-img-top" src="../assets/img/300.png" alt="Card image cap">
+
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Clara, 26</h5>
+                        <ul>
+                            <li>Le havre, 76</li>
+                            <li>membre vérifié</li>
+                            <li>Disponible en ligne</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 pb-5">
+                <div class="card">
+                    <img class="card-img-top" src="../assets/img/300.png" alt="Card image cap">
+
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Clara, 26</h5>
+                        <ul>
+                            <li>Le havre, 76</li>
+                            <li>membre vérifié</li>
+                            <li>Disponible en ligne</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 pb-5">
+                <div class="card">
+                    <img class="card-img-top" src="../assets/img/300.png" alt="Card image cap">
+
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Clara, 26</h5>
+                        <ul>
+                            <li>Le havre, 76</li>
+                            <li>membre vérifié</li>
+                            <li>Disponible en ligne</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 pb-5">
+                <div class="card">
+                    <img class="card-img-top" src="../assets/img/300.png" alt="Card image cap">
+
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Clara, 26</h5>
+                        <ul>
+                            <li>Le havre, 76</li>
+                            <li>membre vérifié</li>
+                            <li>Disponible en ligne</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <?php // include("views/login.php") 
+    ?>
+
+    <?php // include("views/register.php") 
+    ?>
+
+    <?php include("footer.php"); ?>
+
+</div>
+
+<!-- FIN DE PAGE -->
+<?php include("core_views/end.php"); ?>
