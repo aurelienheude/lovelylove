f0cf4cb55d785bb9099fb0d8db7e86b4b966283b
alexis <vauchel.alexis@hotmail.fr>
2021-01-27 21:04:54 +0100

    les tableaux de la mort

--- controllers/lovers_controller.php
+++ controllers/lovers_controller.php
@@ -10,7 +10,7 @@ $tab_all_member =
             "gender" => "homme",
             "mail" => "marius@gmail.com",
             "zipcode" => 76620,
-            "picture" => "../assets/img/300.png",
+            "picture" => "../assets/img/1.png",
             "description" => "je mange des cookies et j'aime le foot et boire des grosses binouse"
         ],
         [
@@ -20,8 +20,188 @@ $tab_all_member =
             "gender" => "homme",
             "mail" => "toto@gmail.com",
             "zipcode" => 76620,
-            "picture" => "../assets/img/300.png",
+            "picture" => "../assets/img/2.png",
             "description" => "je mange des cookies"
+        ],
+        [
+            "lastname" => "pierre",
+            "firstname" => "pauljacques",
+            "age" => 26,
+            "gender" => "homme",
+            "mail" => "ppj@gmail.com",
+            "zipcode" => 76620,
+            "picture" => "../assets/img/3.png",
+            "description" => "J’adore les magasines de pêche surtout s’ils sont spécialisés sur la pêche en rivière."
+        ],
+        [
+            "lastname" => "jenedirais",
+            "firstname" => "pas mon nom",
+            "age" => 18,
+            "gender" => "homme",
+            "mail" => "anonyme@gmail.com",
+            "zipcode" => 76600,
+            "picture" => "../assets/img/4.png",
+            "description" => "Wesh g envie de m'ammuser T ok"
+        ],
+        [
+            "lastname" => "Manu",
+            "firstname" => "bouchon",
+            "age" => 35,
+            "gender" => "homme",
+            "mail" => "manu.b@gmail.com",
+            "zipcode" => 76600,
+            "picture" => "../assets/img/5.png",
+            "description" => "Recherche une femme blonde de 1m75 précis."
+        ],
+        [
+            "lastname" => "pierre",
+            "firstname" => "papiercizo",
+            "age" => 27,
+            "gender" => "homme",
+            "mail" => "manu.b@gmail.com",
+            "zipcode" => 76430,
+            "picture" => "../assets/img/6.png",
+            "description" => "Je suis drôle, très doué en sport, j’aime beaucoup le tennis et j’ai fait de grandes études."
+        ],
+        [
+            "lastname" => "aurelien",
+            "firstname" => "naqa",
+            "age" => 25,
+            "gender" => "homme",
+            "mail" => "naqa@gmail.com",
+            "zipcode" => 76600,
+            "picture" => "../assets/img/7.png",
+            "description" => "Pour moi, les femmes n’ont pas de secret. Elles sont toutes à mes pieds, personne ne peut me résister."
+        ],
+        [
+            "lastname" => "anousone",
+            "firstname" => "formateur",
+            "age" => 28,
+            "gender" => "homme",
+            "mail" => "phpformation@gmail.com",
+            "zipcode" => 76600,
+            "picture" => "../assets/img/8.png",
+            "description" => "Cherche une partenaire de voyage qui n’a pas peur de l’aventure."
+        ],
+        [
+            "lastname" => "christopher",
+            "firstname" => "dumontel",
+            "age" => 24,
+            "gender" => "homme",
+            "mail" => "chriso.d@gmail.com",
+            "zipcode" => 76640,
+            "picture" => "../assets/img/9.png",
+            "description" => "Si tu cherches mieux que ton ex, je peux être ta perle rare."
+        ],
+        [
+            "lastname" => "paul",
+            "firstname" => "ledernier",
+            "age" => 36,
+            "gender" => "homme",
+            "mail" => "popaul@gmail.com",
+            "zipcode" => 76640,
+            "picture" => "../assets/img/10.png",
+            "description" => "J’adore la nature et les découvertes, je suis curieux mais sans prétentions"
+        ],
+        [
+            "lastname" => "sarah",
+            "firstname" => "croche",
+            "age" => 26,
+            "gender" => "femme",
+            "mail" => "sarah.cl@gmail.com",
+            "zipcode" => 76600,
+            "picture" => "../assets/img/11.png",
+            "description" => "Petit coeur fragile recherchant un prince"
+        ],
+        [
+            "lastname" => "johanna",
+            "firstname" => "pietre",
+            "age" => 24,
+            "gender" => "femme",
+            "mail" => "sjojohanna@gmail.com",
+            "zipcode" => 76600,
+            "picture" => "../assets/img/12.png",
+            "description" => "Si tu n’as rien dans le cerveau, ne vient pas me parler"
+        ],
+        [
+            "lastname" => "sandrine",
+            "firstname" => "célong",
+            "age" => 28,
+            "gender" => "femme",
+            "mail" => "s.celong@gmail.com",
+            "zipcode" => 76640,
+            "picture" => "../assets/img/13.png",
+            "description" => "Jeune femme recherche à se faire plaisir en toute liberté "
+        ],
+        [
+            "lastname" => "helene",
+            "firstname" => "trolong",
+            "age" => 20,
+            "gender" => "femme",
+            "mail" => "doudou7698Amour@gmail.com",
+            "zipcode" => 76640,
+            "picture" => "../assets/img/14.png",
+            "description" => " La semaine dernière à New York, le mois prochain à Istanbul. Si toi aussi tu as l’âme voyageuse, viens donc me parler"
+        ],
+        [
+            "lastname" => "cherie",
+            "firstname" => "coco",
+            "age" => 25,
+            "gender" => "femme",
+            "mail" => "cheriecocofautpasblaguer@gmail.com",
+            "zipcode" => 76430,
+            "picture" => "../assets/img/15.png",
+            "description" => "Préfère les longues conversations plutôt que les rencontres superficielles"
+        ],
+        [
+            "lastname" => "ariel",
+            "firstname" => "ptitesirene",
+            "age" => 25,
+            "gender" => "femme",
+            "mail" => "plusdidee@gmail.com",
+            "zipcode" => 76430,
+            "picture" => "../assets/img/16.png",
+            "description" => "Est-ce que tu as décidé de passer ta vie avec des losers comme ton ex ou plutôt de changer le monde avec moi ?"
+        ],
+        [
+            "lastname" => "marie",
+            "firstname" => "stusavais",
+            "age" => 22,
+            "gender" => "femme",
+            "mail" => "marie.st@gmail.com",
+            "zipcode" => 76430,
+            "picture" => "../assets/img/17.png",
+            "description" => "À la recherche de quelqu’un pour pleurer avec moi devant les films romantiques"
+        ],
+        [
+            "lastname" => "nickie",
+            "firstname" => "chiouaoua",
+            "age" => 24,
+            "gender" => "femme",
+            "mail" => "marie.st@gmail.com",
+            "zipcode" => 76640,
+            "picture" => "../assets/img/18.png",
+            "description" => "Tacos, Dormir, Tacos, Aller au travail, Tacos, Faire du sport, Tacos, Regarder un film, Tacos … on mange ensemble ? "
+        ],
+        [
+            "lastname" => "justine",
+            "firstname" => "dupres",
+            "age" => 30,
+            "gender" => "femme",
+            "mail" => "j.dupres@gmail.com",
+            "zipcode" => 76640,
+            "picture" => "../assets/img/19.png",
+            "description" => "On dira à tes parents qu’on s’est rencontrés dans une bibliothèque "
+        ],
+        [
+            "lastname" => "sylvie",
+            "firstname" => "finger",
+            "age" => 18,
+            "gender" => "femme",
+            "mail" => "fingerinthenose@gmail.com",
+            "zipcode" => 76600,
+            "picture" => "../assets/img/20.png",
+            "description" => "Grande fan de taxidermie et de peaux animales "
         ]
 
     ];
