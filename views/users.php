17f67e0941f097f6c81eafc6894c13cd2b1988c4
aurelienheude <aurelienheudepro@gmail.com>
2021-01-27 23:22:07 +0100

    supression de user off pas le temps de niaisé

--- views/users.php
+++ views/users.php
@@ -7,74 +7,84 @@
 
 <div class="container vh-100">
 
+<!--------------------------------------------------------------------------->
+<!--------------------------------------------------------------------------->
+<!--------------------------------------------------------------------------->
+
 	<?php include("nav.php") ?>
 
-	<section class="bg-success container d-flex m-0 p-0">
-		<div class="row mx-auto">
-			<div class="col-md-12 py-5">
-				<img class="Profil_pic_circle" src="../assets/img/logo.png" />
-			</div>
-		</div>
-	</section>
+<!--------------------------------------------------------------------------->
+<!--------------------------------------------------------------------------->
+<!--------------------------------------------------------------------------->
+
+	<?php include("banner_user.php") ?>
+
+<!--------------------------------------------------------------------------->
+<!--------------------------------------------------------------------------->
+<!--------------------------------------------------------------------------->
 
 	<div class="parent-container d-flex bg-success">
-    <div class="container w-25">
-        <div class="row h-100">
-            <div class="col-md-12 h-25">
-				<img class="Profil_pic_circle" src="../assets/img/logo.png" />
-            </div>
-			<div class="col-md-12">
-				<div class="list-group" id="list-tab" role="tablist">
-					<a class="list-group-item list-group-item-action" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Home</a>
-					<a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Profile</a>
+		<div class="container w-25">
+			<div class="row h-100">
+				<div class="col-md-12">
+
 				</div>
-            </div>
-        </div>
-    </div>
+			</div>
+		</div>
+		<div class="container p-5 w-75">
+			<div class="row h-100">
+				<div class="col-md-12 text-center">
+					aaa
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="parent-container d-flex bg-success">
+		<div class="container w-25">
+			<div class="row h-100">
+				<div class="col-md-12 h-25">
+					<img class="Profil_pic_circle" src="../assets/img/logo.png" />
+				</div>
+				<div class="col-md-12">
+					<div class="list-group" id="list-tab" role="tablist">
+						<a class="list-group-item list-group-item-action" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Home</a>
+						<a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Profile</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container w-75 bg-primary">
+			<div class="row">
+				<div class="col-md-12 text-center ">
+					<ul class="list-group">
+						<li class="list-group-item">Nom : <?php echo $_COOKIE["lastname"]; ?></li>
+						<li class="list-group-item">Prénom : <?php echo $_COOKIE["firstname"]; ?></li>
+						<li class="list-group-item">age : <?php echo $_COOKIE["age"]; ?></li>
+					</ul>
+					<ul class="list-group">
+						<li class="list-group-item">Genre : <?php echo $_COOKIE["gender"]; ?></li>
+						<li class="list-group-item">email : <?php echo $_COOKIE["mail"]; ?></li>
+						<li class="list-group-item">Code postal : <?php echo $_COOKIE["zipcode"]; ?></li>
+					</ul>
+				</div>
+				<div class="col-md-12">
 
-    <div class="container w-75 bg-primary">
-        <div class="row">
-            <div class="col-md-12 text-center ">
-                <ul class="list-group">
-				<li class="list-group-item">Nom : <?php echo $_COOKIE[""]; ?></li>
-				<li class="list-group-item">Prénom : <?php echo $_COOKIE[""]; ?></li>
-				<li class="list-group-item">age : <?php echo $_COOKIE[""]; ?></li>
-				</ul>
-				<ul class="list-group">
-					<li class="list-group-item">Genre : <?php echo $_COOKIE[""]; ?></li>
-					<li class="list-group-item">email : <?php echo $_COOKIE[""]; ?></li>
-					<li class="list-group-item">Code postal : <?php echo $_COOKIE[""]; ?></li>
-				</ul>
-            </div>
-			<div class="col-md-12">
-                
-            </div>
-        </div>
-    </div>
-</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	<?php // include("views/login.php") ?>
-
-	<?php // include("views/register.php") ?>
+<!--------------------------------------------------------------------------->
+<!--------------------------------------------------------------------------->
+<!--------------------------------------------------------------------------->
 
 	<?php //include("footer.php"); ?>
+
+<!--------------------------------------------------------------------------->
+<!--------------------------------------------------------------------------->
+<!--------------------------------------------------------------------------->
 
 </div>
 
 <!-- FIN DE PAGE -->
-<?php include ("core_views/end.php"); ?>
+<?php include("core_views/end.php"); ?>
