f68c42cb365db5c077a8104b970120348b589f6e
aurelienheude <aurelienheudepro@gmail.com>
2021-01-27 10:55:38 +0100

    correctif nav

--- views/nav.php
+++ views/nav.php
@@ -1,11 +1,11 @@
-<!-- Image and text -->
-<nav class="navbar navbar-light bg-light">
-  <a class="navbar-brand" href="#">
-    <img src="../assets/img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
-    Lovely love
-  </a>
-  <div class="my-2 my-lg-0">
-    <a class="navbar-brand" href="">Accueil</a>
-    <a class="navbar-brand" href="">A propos</a>
-  </div>
-</nav>
+	<!-- Image and text -->
+	<nav class="container m-0 navbar navbar-light bg-light">
+		<div class="row mx-auto">
+			<div class="col-md-12">
+				<a class="navbar-brand" href="#">
+				<img src="../assets/img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+				<span>Lovely love</span>
+				</a>
+			</div>
+		</div>
+	</nav>
