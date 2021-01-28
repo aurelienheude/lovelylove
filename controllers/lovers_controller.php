43ed4e2b0c348713c1511483bc28900c17874568
aurelienheude <aurelienheudepro@gmail.com>
2021-01-28 16:00:06 +0100

    système de tri par cookie interest

--- controllers/lovers_controller.php
+++ controllers/lovers_controller.php
@@ -206,23 +206,291 @@ $tab_all_member =
 
     ];
 
-foreach ($tab_all_member as $key => $value) {
+$tab_men_member =
+
+    [
+        [
+            "lastname" => "Philippe",
+            "firstname" => "candeloro",
+            "age" => 23,
+            "gender" => "homme",
+            "mail" => "marius@gmail.com",
+            "zipcode" => 76620,
+            "picture" => "../assets/img/avatar/1.jpg",
+            "description" => "je mange des cookies et j'aime le foot et boire des grosses binouse"
+        ],
+        [
+            "lastname" => "maxime",
+            "firstname" => "delarue",
+            "age" => 20,
+            "gender" => "homme",
+            "mail" => "toto@gmail.com",
+            "zipcode" => 76620,
+            "picture" => "../assets/img/avatar/2.jpg",
+            "description" => "je mange des cookies"
+        ],
+        [
+            "lastname" => "pierre",
+            "firstname" => "pauljacques",
+            "age" => 26,
+            "gender" => "homme",
+            "mail" => "ppj@gmail.com",
+            "zipcode" => 76620,
+            "picture" => "../assets/img/avatar/3.jpg",
+            "description" => "J’adore les magasines de pêche surtout s’ils sont spécialisés sur la pêche en rivière."
+        ],
+        [
+            "lastname" => "jenedirais",
+            "firstname" => "pas mon nom",
+            "age" => 18,
+            "gender" => "homme",
+            "mail" => "anonyme@gmail.com",
+            "zipcode" => 76600,
+            "picture" => "../assets/img/avatar/4.jpg",
+            "description" => "Wesh g envie de m'ammuser T ok"
+        ],
+        [
+            "lastname" => "Manu",
+            "firstname" => "bouchon",
+            "age" => 35,
+            "gender" => "homme",
+            "mail" => "manu.b@gmail.com",
+            "zipcode" => 76600,
+            "picture" => "../assets/img/avatar/5.jpg",
+            "description" => "Recherche une femme blonde de 1m75 précis."
+        ],
+        [
+            "lastname" => "pierre",
+            "firstname" => "papiercizo",
+            "age" => 27,
+            "gender" => "homme",
+            "mail" => "manu.b@gmail.com",
+            "zipcode" => 76430,
+            "picture" => "../assets/img/avatar/6.jpg",
+            "description" => "Je suis drôle, très doué en sport, j’aime beaucoup le tennis et j’ai fait de grandes études."
+        ],
+        [
+            "lastname" => "aurelien",
+            "firstname" => "naqa",
+            "age" => 25,
+            "gender" => "homme",
+            "mail" => "naqa@gmail.com",
+            "zipcode" => 76600,
+            "picture" => "../assets/img/avatar/7.jpg",
+            "description" => "Pour moi, les femmes n’ont pas de secret. Elles sont toutes à mes pieds, personne ne peut me résister."
+        ],
+        [
+            "lastname" => "anousone",
+            "firstname" => "formateur",
+            "age" => 28,
+            "gender" => "homme",
+            "mail" => "phpformation@gmail.com",
+            "zipcode" => 76600,
+            "picture" => "../assets/img/avatar/8.jpg",
+            "description" => "Cherche une partenaire de voyage qui n’a pas peur de l’aventure."
+        ],
+        [
+            "lastname" => "christopher",
+            "firstname" => "dumontel",
+            "age" => 24,
+            "gender" => "homme",
+            "mail" => "chriso.d@gmail.com",
+            "zipcode" => 76640,
+            "picture" => "../assets/img/avatar/9.jpg",
+            "description" => "Si tu cherches mieux que ton ex, je peux être ta perle rare."
+        ],
+        [
+            "lastname" => "paul",
+            "firstname" => "ledernier",
+            "age" => 36,
+            "gender" => "homme",
+            "mail" => "popaul@gmail.com",
+            "zipcode" => 76640,
+            "picture" => "../assets/img/avatar/10.jpg",
+            "description" => "J’adore la nature et les découvertes, je suis curieux mais sans prétentions"
+        ]
+    ];
+
+$tab_women_member =
+
+    [
+        [
+            "lastname" => "sarah",
+            "firstname" => "croche",
+            "age" => 26,
+            "gender" => "femme",
+            "mail" => "sarah.cl@gmail.com",
+            "zipcode" => 76600,
+            "picture" => "../assets/img/avatar/11.jpg",
+            "description" => "Petit coeur fragile recherchant un prince"
+        ],
+        [
+            "lastname" => "johanna",
+            "firstname" => "pietre",
+            "age" => 24,
+            "gender" => "femme",
+            "mail" => "sjojohanna@gmail.com",
+            "zipcode" => 76600,
+            "picture" => "../assets/img/avatar/12.jpg",
+            "description" => "Si tu n’as rien dans le cerveau, ne vient pas me parler"
+        ],
+        [
+            "lastname" => "sandrine",
+            "firstname" => "célong",
+            "age" => 28,
+            "gender" => "femme",
+            "mail" => "s.celong@gmail.com",
+            "zipcode" => 76640,
+            "picture" => "../assets/img/avatar/13.jpg",
+            "description" => "Jeune femme recherche à se faire plaisir en toute liberté "
+        ],
+        [
+            "lastname" => "helene",
+            "firstname" => "trolong",
+            "age" => 20,
+            "gender" => "femme",
+            "mail" => "doudou7698Amour@gmail.com",
+            "zipcode" => 76640,
+            "picture" => "../assets/img/avatar/14.jpg",
+            "description" => " La semaine dernière à New York, le mois prochain à Istanbul. Si toi aussi tu as l’âme voyageuse, viens donc me parler"
+        ],
+        [
+            "lastname" => "cherie",
+            "firstname" => "coco",
+            "age" => 25,
+            "gender" => "femme",
+            "mail" => "cheriecocofautpasblaguer@gmail.com",
+            "zipcode" => 76430,
+            "picture" => "../assets/img/avatar/15.jpg",
+            "description" => "Préfère les longues conversations plutôt que les rencontres superficielles"
+        ],
+        [
+            "lastname" => "ariel",
+            "firstname" => "ptitesirene",
+            "age" => 25,
+            "gender" => "femme",
+            "mail" => "plusdidee@gmail.com",
+            "zipcode" => 76430,
+            "picture" => "../assets/img/avatar/16.jpg",
+            "description" => "Est-ce que tu as décidé de passer ta vie avec des losers comme ton ex ou plutôt de changer le monde avec moi ?"
+        ],
+        [
+            "lastname" => "marie",
+            "firstname" => "stusavais",
+            "age" => 22,
+            "gender" => "femme",
+            "mail" => "marie.st@gmail.com",
+            "zipcode" => 76430,
+            "picture" => "../assets/img/avatar/17.jpg",
+            "description" => "À la recherche de quelqu’un pour pleurer avec moi devant les films romantiques"
+        ],
+        [
+            "lastname" => "nickie",
+            "firstname" => "chiouaoua",
+            "age" => 24,
+            "gender" => "femme",
+            "mail" => "marie.st@gmail.com",
+            "zipcode" => 76640,
+            "picture" => "../assets/img/avatar/18.jpg",
+            "description" => "Tacos, Dormir, Tacos, Aller au travail, Tacos, Faire du sport, Tacos, Regarder un film, Tacos … on mange ensemble ? "
+        ],
+        [
+            "lastname" => "justine",
+            "firstname" => "dupres",
+            "age" => 30,
+            "gender" => "femme",
+            "mail" => "j.dupres@gmail.com",
+            "zipcode" => 76640,
+            "picture" => "../assets/img/avatar/19.jpg",
+            "description" => "On dira à tes parents qu’on s’est rencontrés dans une bibliothèque "
+        ],
+        [
+            "lastname" => "sylvie",
+            "firstname" => "finger",
+            "age" => 18,
+            "gender" => "femme",
+            "mail" => "fingerinthenose@gmail.com",
+            "zipcode" => 76600,
+            "picture" => "../assets/img/avatar/20.jpg",
+            "description" => "Grande fan de taxidermie et de peaux animales "
+        ]
+
+    ];
+
+
+if ($_COOKIE["interest"] == "Homme") {
+    foreach ($tab_men_member as $key => $value) {
+?>
+
+        <div class="col-md-3 pb-5">
+            <div class="card">
+                <img class="card-img-top" src="<?php echo $value["picture"]; ?>" alt="Card image cap">
+
+                <div class="card-body">
+                    <h5 class="card-title text-center"><?php echo $value["lastname"] . " " . $value["firstname"]; ?></h5>
+                    <ul>
+                        <li>age : <?php echo $value["age"]; ?></li>
+                        <li>code postal : <?php echo $value["zipcode"]; ?></li>
+                        <li>Description : <?php echo $value["description"]; ?></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+<?php
+    }
+}
+?>
+<?php
+if ($_COOKIE["interest"] == "Femme") {
+    foreach ($tab_women_member as $key => $value) {
+?>
+
+
+        <div class="col-md-3 pb-5">
+            <div class="card">
+                <img class="card-img-top" src="<?php echo $value["picture"]; ?>" alt="Card image cap">
+
+                <div class="card-body">
+                    <h5 class="card-title text-center"><?php echo $value["lastname"] . " " . $value["firstname"]; ?></h5>
+                    <ul>
+                        <li>age : <?php echo $value["age"]; ?></li>
+                        <li>code postal : <?php echo $value["zipcode"]; ?></li>
+                        <li>Description : <?php echo $value["description"]; ?></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+<?php
+    }
+}
+?>
+
+<?php
+
+if ($_COOKIE["interest"] == "tout") {
+
+    foreach ($tab_all_member as $key => $value) {
 
 ?>
 
-    <div class="col-md-3 pb-5">
-        <div class="card">
-            <img class="card-img-top" src="<?php echo $value["picture"]; ?>" alt="Card image cap">
+        <div class="col-md-3 pb-5">
+            <div class="card">
+                <img class="card-img-top" src="<?php echo $value["picture"]; ?>" alt="Card image cap">
 
-            <div class="card-body">
-                <h5 class="card-title text-center"><?php echo $value["lastname"] . " " . $value["firstname"]; ?></h5>
-                <ul>
-                    <li>age : <?php echo $value["age"]; ?></li>
-                    <li>code postal : <?php echo $value["zipcode"]; ?></li>
-                    <li>Description : <?php echo $value["description"]; ?></li>
-                </ul>
+                <div class="card-body">
+                    <h5 class="card-title text-center"><?php echo $value["lastname"] . " " . $value["firstname"]; ?></h5>
+                    <ul>
+                        <li>age : <?php echo $value["age"]; ?></li>
+                        <li>code postal : <?php echo $value["zipcode"]; ?></li>
+                        <li>Description : <?php echo $value["description"]; ?></li>
+                    </ul>
+                </div>
             </div>
         </div>
-    </div>
 
-<?php } ?>
+<?php
+    }
+}
+?>
