75614d2f85aab5a7a676b9abc1754d1f0631018b
alexis <vauchel.alexis@hotmail.fr>
2021-01-29 09:19:45 +0100

    modification leger pour redirection en cas de cookies eronnés

--- controllers/redirect.php
+++ controllers/redirect.php
@@ -4,7 +4,10 @@ if(
 ||  isset($_COOKIE['lastname']) == False
 ||  isset($_COOKIE['gender']) == False 
 ||  isset($_COOKIE['mail']) == False
-||  isset($_COOKIE['zipcode']) == False)
+||  isset($_COOKIE['zipcode']) == False
+||  isset($_COOKIE['age']) == False
+||  isset($_COOKIE['interest']) == False
+||  isset($_COOKIE['search']) == False)
 
 {
     header("Location: ../index.php");
